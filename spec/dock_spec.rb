36fb18c05099b362b0822200b1cdf4f3459907be
Tanner Dale <tanner@tannerdale.com>
2021-06-21 23:47:40 -0500

    Add doesnt charge over max rental hours test

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -52,4 +52,28 @@ RSpec.describe 'Dock' do
     expect(charge[:card_number]).to eq(patrick.credit_card_number)
     expect(charge[:amount]).to eq(40)
   end
+
+  it 'doesnt charge for more than 3 hours' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+
+    kayak_1.add_hour
+    kayak_1.add_hour
+    kayak_1.add_hour
+
+    charge = dock.charge(kayak_1)
+    expect(charge[:amount]).to eq(60)
+
+    kayak_1.add_hour
+
+    charge = dock.charge(kayak_1)
+    expect(charge[:amount]).to eq(60)
+  end
 end
