0c1f42ca52ee2d55cba23fea4974c5a734d172a6
Tanner Dale <tanner@tannerdale.com>
2021-06-21 23:45:27 -0500

    Add charge method

--- lib/dock.rb
+++ lib/dock.rb
@@ -13,4 +13,14 @@ class Dock
   def rent(boat, renter)
     @rental_log[boat] = renter
   end
+
+  def charge(boat)
+    card_number = rental_log[boat].credit_card_number
+    amount = boat.price_per_hour * (boat.hours_rented <= 3 ? boat.hours_rented : 3)
+
+    {
+      card_number: card_number,
+      amount: amount
+    }
+  end
 end
