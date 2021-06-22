8df9846d97885152ec08bb63558b120473ef2a6a
Tanner Dale <tanner@tannerdale.com>
2021-06-21 23:45:07 -0500

    Add charge method test

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -32,4 +32,24 @@ RSpec.describe 'Dock' do
     }
     expect(dock.rental_log).to eq(expected)
   end
+
+  it 'can charge a renter' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+
+    kayak_1.add_hour
+    kayak_1.add_hour
+
+    charge = dock.charge(kayak_1)
+
+    expect(charge[:card_number]).to eq(patrick.credit_card_number)
+    expect(charge[:amount]).to eq(40)
+  end
 end
